589d17947075015ce6b3ff25ca1219d556045ac1
Smit Trivedi <smit.iihglobal@gmail.com>
2025-04-02 16:45:06 +0530

    extra css removed!

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>    
+      <h1>Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
